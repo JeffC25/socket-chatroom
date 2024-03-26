c5f496200a494076733a8f0ecb22e61983219727
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2024-03-26 10:42:52 -0400

    fix error messages

--- cmd/main.go
+++ cmd/main.go
@@ -11,18 +11,18 @@ func main() {
 
 	ln, err := net.Listen("tcp", ":8000")
 	if err != nil {
-		fmt.Errorf("Error binding to port")
+		fmt.Println("Error binding to port")
 	}
 
 	conn, err := ln.Accept()
 	if err != nil {
-		fmt.Errorf("Error accepting connection")
+		fmt.Println("Error accepting connection")
 	}
 
 	for {
 		msg, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
-			fmt.Errorf("Error reading message")
+			fmt.Println("Error reading message")
 		} else {
 			fmt.Print("Message Received:", string(msg))
 
